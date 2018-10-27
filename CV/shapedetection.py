95c60306898ecf69b178cb9e59450a9a37732238
Senpat <pgz11902@gmail.com>
2018-10-27 11:45:11 -0400

    adapt matchshapes to dronethreshcontourlaptopshape2.py

--- CV/shapedetection.py
+++ CV/shapedetection.py
@@ -13,14 +13,14 @@ image = cv2.imread(args["image"])
 
 TRIANGLE = cv2.imread("Shapes/TRIANGLE.png")
 SQUARE = cv2.imread("Shapes/SQUARE.png")
-SQUARE = cv2.bitwise_not(SQUARE)
+
 image = cv2.bitwise_not(image)
 
 TRIANGLE = cv2.cvtColor(TRIANGLE, cv2.COLOR_BGR2GRAY)
 SQUARE = cv2.cvtColor(SQUARE, cv2.COLOR_BGR2GRAY)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-TRIANGLECNTS= cv2.findContours(TRIANGLE,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
+TRIANGLECNTS= cv2.findContours(TRIANGLE,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 SQUARECNTS = cv2.findContours(SQUARE,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 imagecnts = cv2.findContours(image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 
@@ -35,3 +35,5 @@ cv2.imshow("Square",SQUARE)
 cv2.imshow("Image",image)
 
 cv2.waitKey(20000)
+
+cv2.imwrite("Shapes/SQUARE.png",SQUARE)
