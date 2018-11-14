00de0870417e90939dd7a043e22c5ba4000235e3
Senpat <pgz11902@gmail.com>
2018-11-14 15:21:08 -0500

    mission updates

--- FC/mission.py
+++ FC/mission.py
@@ -7,11 +7,12 @@ import logging
 log = logging.getLogger('drone script')
 startime = time.time()
 log.info ("Connecting")
-#connection_string='127.0.0.1:14550'
-connection_string = '/dev/ttyS0'
+#print("Connecting")
+connection_string='tcp:127.0.0.1:5760'
+#connection_string = '/dev/ttyS0'
 arglist = ['parameters','gps_0','armed','mode','attitude','system_status','location']
-vehicle = dk.connect(connection_string, wait_ready = arglist, heartbeat_timeout = 300, baud = 57600)
-
+#vehicle = dk.connect(connection_string, wait_ready = arglist, heartbeat_timeout = 300, baud = 57600)
+vehicle = dk.connect(connection_string,wait_ready=True)
 log.info("Time to connection: %s" % str(time.time()-startime))
 
 cmds = vehicle.commands
@@ -23,6 +24,6 @@ cmds.wait_ready() #wait untuil downlaod is done
 missionlist = [cmds] #put current command list into missionlist
 missionlist[0].command=mavutil.mavlink.MAV_CMD_NAV_TAKEOFF #chaange first command to takeoff
 cmds.clear() #clear vehicles previous list of commands(mission)
-for i in missionlist
+for i in missionlist:
     cmds.add(i)
 cmds.upload()
