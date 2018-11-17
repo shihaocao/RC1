9c3c67ab71f9b4c3592d2ed1a951fb795cdd833e
Ganesh <ganesh.nanduru@gmail.com>
2018-11-17 12:29:19 -0500

    streaming attempt

--- COMMUNICATION/tcp_sendToCannyVideo.py
+++ COMMUNICATION/tcp_sendToCannyVideo.py
@@ -5,7 +5,7 @@ import cv2
 import numpy
 import imutils
 
-TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.1.245'
 TCP_PORT = 5005
 pause = "PAUSE"
 quit = "QUIT"
@@ -17,12 +17,46 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_STREAM) # TCP
 sock.connect((TCP_IP, TCP_PORT))
 
-print("p = pause\nq = quit\nx = end program\ns = save image\ni = send image")
+print("p = pause\nq = quit\nx = end program\ns = save image\ni = send image\nt = stream")
 data = input("Please enter command:")
+
+def frame():
+    sock.send("DIMENSIONS")
+    start = time.time()
+
+    dimensions_data = sock.recv(100)
+    print(dimensions_data)
+    sock.send("FRAME".encode('utf-8'))
+    [real_width,real_height,width,height] = [int(i) for i in dimensions_data.decode('utf-8').split(',')]
+    print(real_width,real_height,width,height)
+
+    buffer = width*height*10 + 10 #10000 just incase extra data somehow gets sent
+    print(buffer)
+    start1 = time.time()
+    image_data = sock.recv(buffer)
+    print(time.time())
+    end1 = time.time()
+    print("Receiving time: ", str(end1-start1))
+    print(len(image_data))
+    start2 = time.time()
+    fullimage = []
+    row = []
+    nparr = numpy.frombuffer(image_data, numpy.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    fullimage = imutils.resize(img, width=500)
+    end2 = time.time()
+    print("Process: ", str(end2-start2))
+#            cv2.imwrite('createdFromTCP'+str(j)+'.png',fullimage)
+#            image_read = cv2.imread('createdFromTCP'+str(j)+'.png')
+    cv2.imshow('createdFromTCP',fullimage)
+    cv2.waitKey(1)
+
 #Commands legend: p = pause, q = quit video, x = end program, s = save image file
 while len(data) > 0:
     if data == "p":
         sock.send(pause.encode('utf-8'))
+    elif data == 't':
+        sock.send('STREAM'.encode('utf-8'))
     elif data == "q":
         sock.send(quit.encode('utf-8'))
     elif data == "x":
@@ -30,43 +64,10 @@ while len(data) > 0:
     elif data == "s":
         sock.send(save_image.encode('utf-8'))
     elif data == "i":
-        sock.send(send_image.encode('utf-8'))
-        data1 = input("How many frames:")
-        frame_num = int(data1)
-        data = sock.recv(50)
-        sock.send(data1.encode("utf-8"))
-        data = sock.recv(50)
+        seconds = int(input('how many seconds  '))
         start = time.time()
-        for j in range(frame_num):
-            print('hi')
-            sock.send("READY".encode('utf-8'))
-            dimensions_data = sock.recv(100)
-            print(dimensions_data)
-            [real_width,real_height,width,height] = [int(i) for i in dimensions_data.decode('utf-8').split(',')]
-            print(real_width,real_height,width,height)
-
-            buffer = width*height*10 + 10 #10000 just incase extra data somehow gets sent
-            print(buffer)
-            start1 = time.time()
-            image_data = sock.recv(buffer)
-            print(time.time())
-            end1 = time.time()
-            print("Receiving time: ", str(end1-start1))
-            print(len(image_data))
-            start2 = time.time()
-            fullimage = []
-            row = []
-            nparr = numpy.fromstring(image_data, numpy.uint8)
-            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-            fullimage = imutils.resize(img, width=500)
-            end2 = time.time()
-            print("Process: ", str(end2-start2))
-#            cv2.imwrite('createdFromTCP'+str(j)+'.png',fullimage)
-#            image_read = cv2.imread('createdFromTCP'+str(j)+'.png')
-            cv2.imshow('createdFromTCP',fullimage)
-            cv2.waitKey(1)
-        cv2.destroyAllWindows()
-        end = time.time()
-        print('Time Taken: ',str(end-start))
-        print("Average Time: ", str((end-start)/frame_num))
+        while time.time()-start < seconds:
+            frame()
+        sock.sendAll("ALLDONE")
     data = input("Please enter command:")
+    cv2.destroyAllWindows()
